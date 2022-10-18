4ef03115e49b9155778abf66bd5bcb58ee19482b
q2q2q8 <art-kash@mail.ru>
2022-10-18 18:20:41 +0300

    седьмое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
         var weightFirstBoxer = 78.2;
         var weightSecondBoxer = 82.7;
-        var totalWeight = weightFirstBoxer + weightSecondBoxer;
-        System.out.println("Общий вес боксеров " + totalWeight + " кг");
-        var weightDifference = (weightFirstBoxer - weightSecondBoxer) * (-1);
-        System.out.println("Разница в весе боксеров " + weightDifference + " кг");
+        var weightDifference1 = weightSecondBoxer - weightFirstBoxer;
+        System.out.println(weightDifference1);
+        var weightDifference2 = weightSecondBoxer % weightFirstBoxer;
+        System.out.println(weightDifference1);
+
 
 
     }
