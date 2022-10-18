e6912e8ca98d2b22b64fbe428d71071761e61144
q2q2q8 <art-kash@mail.ru>
2022-10-18 17:51:56 +0300

    четверое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
-        var cat = 3.6;
-        var paper = 763789;
-        dog = dog - 3;
-        cat = cat - 1.6;
-        paper = paper - 1739;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend +2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
