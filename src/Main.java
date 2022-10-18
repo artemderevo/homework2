ba6911da6eca79ad42cb1a91dd549085da61800a
q2q2q8 <art-kash@mail.ru>
2022-10-18 17:47:49 +0300

    третья задача сделана

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
+        var dog = 8;
         var cat = 3.6;
         var paper = 763789;
-        dog = dog - 3.5;
+        dog = dog - 3;
         cat = cat - 1.6;
         paper = paper - 1739;
         System.out.println(dog);
