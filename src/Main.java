1733c3929132d23256d48244662b1be8ae5b16c7
q2q2q8 <art-kash@mail.ru>
2022-10-18 18:32:58 +0300

    восьмое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        var weightFirstBoxer = 78.2;
-        var weightSecondBoxer = 82.7;
-        var weightDifference1 = weightSecondBoxer - weightFirstBoxer;
-        System.out.println(weightDifference1);
-        var weightDifference2 = weightSecondBoxer % weightFirstBoxer;
-        System.out.println(weightDifference1);
+        var totalHours = 640;
+        var hoursPerEmployee = 8;
+        var amountOfWorkers = totalHours / hoursPerEmployee;
+        System.out.println("Всего работников в компании – " + amountOfWorkers + " человек");
+        var currentValueOfEmployees = amountOfWorkers + 94;
+        var currentHoursPerEmployee = totalHours / currentValueOfEmployees;
+        System.out.println("Если в компании работает " + currentValueOfEmployees + " человек, то всего " + currentHoursPerEmployee + " часов работы может быть поделено между сотрудниками.");
+
+
 
 
 
